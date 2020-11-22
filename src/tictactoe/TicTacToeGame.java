e84517265981d03b9883c63374c5c5c878e490c7
MURA199918 <muralisaranr18@gmail.com>
2020-11-22 19:39:29 +0530

    created UC1

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -1,8 +1,16 @@
 package tictactoe;
 
 public class TicTacToeGame {
+	public char[] arr=new char[10];
+	public void UC1(){
+		for(int i=1;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
+		TicTacToeGame obj1=new TicTacToeGame();
+		obj1.UC1();
 	}
 
 }
