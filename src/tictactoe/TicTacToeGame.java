990036c36588460a8f5a15a2b893dc26e3e94774
MURA199918 <muralisaranr18@gmail.com>
2020-11-22 20:18:25 +0530

    created UC4

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -5,6 +5,7 @@ public class TicTacToeGame {
 	Scanner sc=new Scanner(System.in);
 	public char[] arr=new char[10];
 	public char[][] board=new char[3][3];
+	char pchoice,cchoice;
 	public void UC1(){
 		for(int i=1;i<arr.length;i++) {
 			System.out.println(arr[i]);
@@ -12,15 +13,17 @@ public class TicTacToeGame {
 	}
 	public void playerchoice() {
 		System.out.println("Enter player choice");
-		char pchoice=sc.next().charAt(0);
+		pchoice=sc.next().charAt(0);
 		if(pchoice!='X' || pchoice!='O') {
 			System.out.println("Invalid player choice");
 		}
 		else if(pchoice=='X') {
+			cchoice='O';
 			System.out.println("Player choice is X");
 			System.out.println("Computer choice is O");
 		}
 		else {
+			cchoice='X';
 			System.out.println("Player choice is O");
 			System.out.println("Computer choice is X");
 		}
@@ -33,12 +36,31 @@ public class TicTacToeGame {
 			}
 		}
 	}
+	public void playerindex() {
+		System.out.println("select player index");
+		int pindex=sc.nextInt();
+		if(arr[pindex]==0) {
+			arr[pindex]=pchoice;
+		}
+		else {
+			System.out.println("Invalid player input");
+		}
+		System.out.println("select computer index");
+		int cindex=sc.nextInt();
+		if(arr[cindex]==0) {
+			arr[cindex]=cchoice;
+		}
+		else {
+			System.out.println("Invalid computer input");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj1=new TicTacToeGame();
 		obj1.UC1();
 		obj1.playerchoice();
 		obj1.showboard();
+		obj1.playerindex();
 	}
 
 }
