6b90ab24ed067733ca24cd9724a64de27ffe586a
MURA199918 <muralisaranr18@gmail.com>
2020-11-22 20:05:15 +0530

    created UC3

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class TicTacToeGame {
 	Scanner sc=new Scanner(System.in);
 	public char[] arr=new char[10];
+	public char[][] board=new char[3][3];
 	public void UC1(){
 		for(int i=1;i<arr.length;i++) {
 			System.out.println(arr[i]);
@@ -24,11 +25,20 @@ public class TicTacToeGame {
 			System.out.println("Computer choice is X");
 		}
 	}
+	public void showboard(){
+		System.out.println("Displaying the current board..");
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=3;j++){
+				System.out.println(board[i][j]);
+			}
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj1=new TicTacToeGame();
 		obj1.UC1();
 		obj1.playerchoice();
+		obj1.showboard();
 	}
 
 }
