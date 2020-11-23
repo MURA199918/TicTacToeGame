d4501c7433a3e9bc8a1d555625dbf5bf506eeccc
MURA199918 <muralisaranr18@gmail.com>
2020-11-23 20:18:38 +0530

    added UC4

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -6,11 +6,6 @@ public class TicTacToeGame {
 	public char[] arr=new char[10];
 	public char[][] board=new char[3][3];
 	char pchoice,cchoice;
-	public void UC1(){
-		for(int i=1;i<arr.length;i++) {
-			System.out.println(arr[i]);
-		}
-	}
 	public void playerchoice() {
 		System.out.println("Enter player choice");
 		pchoice=sc.next().charAt(0);
@@ -57,7 +52,6 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj1=new TicTacToeGame();
-		obj1.UC1();
 		obj1.playerchoice();
 		obj1.showboard();
 		obj1.playerindex();
