34c0c663007eda81551a628028d8f5b3ba25815a
MURA199918 <muralisaranr18@gmail.com>
2020-11-23 20:33:13 +0530

    added UC3

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -3,13 +3,7 @@ import java.util.*;
 
 public class TicTacToeGame {
 	Scanner sc=new Scanner(System.in);
-	public char[] arr=new char[10];
-	public char[][] board=new char[3][3];
-	public void UC1(){
-		for(int i=1;i<arr.length;i++) {
-			System.out.println(arr[i]);
-		}
-	}
+	public char[] board=new char[10];
 	public void playerchoice() {
 		System.out.println("Enter player choice");
 		char pchoice=sc.next().charAt(0);
@@ -27,16 +21,17 @@ public class TicTacToeGame {
 	}
 	public void showboard(){
 		System.out.println("Displaying the current board..");
-		for(int i=1;i<=3;i++){
-			for(int j=1;j<=3;j++){
-				System.out.println(board[i][j]);
-			}
-		}
+		System.out.println("|---|---|---|"); 
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |"); 
+        System.out.println("|-----------|"); 
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |"); 
+        System.out.println("|-----------|"); 
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |"); 
+        System.out.println("|---|---|---|"); 
 	}
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj1=new TicTacToeGame();
-		obj1.UC1();
 		obj1.playerchoice();
 		obj1.showboard();
 	}
