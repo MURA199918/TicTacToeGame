a233714201c6180d49f4a2a21feeb2d317a493af
MURA199918 <muralisaranr18@gmail.com>
2020-11-23 20:41:15 +0530

    added UC6

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -3,16 +3,10 @@ import java.util.*;
 
 public class TicTacToeGame {
 	Scanner sc=new Scanner(System.in);
-	public char[] arr=new char[10];
-	public char[][] board=new char[3][3];
+	public char[] board=new char[3];
 	char pchoice,cchoice;
 	int pindex,cindex;
 	String toss;
-	public void UC1(){
-		for(int i=1;i<arr.length;i++) {
-			System.out.println(arr[i]);
-		}
-	}
 	public void playerchoice() {
 		if(toss=="heads") {
 		  System.out.println("Enter player choice");
@@ -51,43 +45,27 @@ public class TicTacToeGame {
 	}
 	public void showboard(){
 		System.out.println("Displaying the current board..");
-		for(int i=0;i<3;i++){
-			for(int j=0;j<3;j++){
-				System.out.println(board[i][j]);
-			}
-		}
+		System.out.println("|---|---|---|"); 
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |"); 
+        System.out.println("|-----------|"); 
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |"); 
+        System.out.println("|-----------|"); 
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |"); 
+        System.out.println("|---|---|---|"); 
 	}
 	public void playerindex() {
 		System.out.println("select player index");
 		pindex=sc.nextInt();
-		if(arr[pindex]==0) {
-			arr[pindex]=pchoice;
-			if(pindex<=3) {
-				board[0][pindex-1]=arr[pindex];
-			}
-			else if(pindex>3 && pindex<=6) {
-				board[1][pindex-4]=arr[pindex];
-			}
-			else {
-				board[2][pindex-7]=arr[pindex];
-			}
+		if(board[pindex]==0) {
+			board[pindex]=pchoice;
 		}
 		else {
 			System.out.println("Invalid player input");
 		}
 		System.out.println("select computer index");
 		cindex=sc.nextInt();
-		if(arr[cindex]==0) {
-			arr[cindex]=cchoice;
-			if(cindex<=3) {
-				board[0][cindex-1]=arr[cindex];
-			}
-			else if(cindex>3 && cindex<=6) {
-				board[1][cindex-4]=arr[cindex];
-			}
-			else {
-				board[2][cindex-7]=arr[cindex];
-			}
+		if(board[cindex]==0) {
+			board[cindex]=cchoice;
 		}
 		else {
 			System.out.println("Invalid computer input");
@@ -106,7 +84,6 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj1=new TicTacToeGame();
-		obj1.UC1();
 		obj1.playerchoice();
 		obj1.showboard();
 		obj1.playerindex();
