ccea7654f86774758facd19724d5216d7b3f5c10
MURA199918 <muralisaranr18@gmail.com>
2020-11-24 09:33:48 +0530

    added UC1

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -1,12 +1,12 @@
 package tictactoe;
 
 public class TicTacToeGame {
-	
-	char[] board=new char[10];
-	
-	public void board() {
-		for(int i=1;i<10;i++) {
-			board[i]=' ';
+
+	char[] board = new char[10];
+
+	public void Createboard() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
 		}
 		System.out.println("Created board");
 	}
@@ -14,7 +14,7 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println(".........Welcome to TicTacToeGame.......");
 		TicTacToeGame obj = new TicTacToeGame();
-		obj.board();
+		obj.Createboard();
 	}
 
 }
